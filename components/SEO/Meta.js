4d6cabf02fff327af44c011885e98fc7b6a96cee
ArnavSingha <102987879+ArnavSingha@users.noreply.github.com>
2025-04-14 22:32:21 +0530

    update

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Arnav Singha's Portfolio - MCA Student</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Arnav Singha's Portfolio - MCA Student" />
+            <meta name="description"
+                content="Arnav Singha's Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Arnav Singha's" />
+            <meta name="keywords"
+                content="Arnav Singha's portfolio, Arnav Singha's linux, ubuntu portfolio, Arnav Singha's protfolio,Arnav Singha's computer, Arnav Singha's, Arnav ubuntu, Arnav Singha ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/as.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Arnav Singha's Portfolio - MCA Student" />
+            <meta itemProp="description"
+                content="Arnav Singha's Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/as.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Arnav Singha's Portfolio - MCA Student" />
+            <meta name="twitter:description"
+                content="Arnav Singha's Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="@007_arnav" />
+            <meta name="twitter:creator" content="@007_arnav" />
+            <meta name="twitter:image:src" content="images/logos/as.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Arnav Singha's Portfolio - MCA Student" />
+            <meta name="og:description"
+                content="Arnav Singha's Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/as.png" />
+            <meta name="og:url" content="https://github.com/ArnavSingha" />
+            <meta name="og:site_name" content="Arnav Singha Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/as.png" />
+            <link rel="apple-touch-icon" href="images/logos/as.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
